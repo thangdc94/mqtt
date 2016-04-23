bcecbc7e0b35f4773c14297a219c3d08d852d3d0
buabuas1 <hailos1000@gmail.com>
2016-04-23 23:48:46 +0700

    fix GUI

--- app/src/main/java/com/example/am/mqtt/MainActivity.java
+++ app/src/main/java/com/example/am/mqtt/MainActivity.java
@@ -2,17 +2,12 @@ package com.example.am.mqtt;
 
 import android.content.SharedPreferences;
 import android.graphics.Color;
-import android.graphics.PorterDuff;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Button;
 import android.widget.CompoundButton;
-import android.widget.Toast;
 import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
@@ -54,33 +49,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
-        MenuItem img = menu.findItem(R.id.action_home);
-        final Drawable drawable = img.getIcon();
-        if (drawable != null) {
-            // If we don't mutate the drawable, then all drawable's with this id will have a color
-            // filter applied to it.
-            drawable.mutate();
-            drawable.setColorFilter(Color.GRAY, PorterDuff.Mode.SRC_ATOP);
-            //drawable.setAlpha(alpha);
-        }
 
-        img.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                if (isOn) {
-                    Toast.makeText(MainActivity.this, "OKe", Toast.LENGTH_SHORT).show();
-                    drawable.mutate();
-                    drawable.setColorFilter(Color.BLACK, PorterDuff.Mode.SRC_ATOP);
-                    isOn = false;
-                    return false;
-                } else {
-                    Toast.makeText(MainActivity.this, "OKe", Toast.LENGTH_SHORT).show();
-                    drawable.mutate();
-                    drawable.setColorFilter(Color.RED, PorterDuff.Mode.SRC_ATOP);
-                    return false;
-                }
-            }
-        });
+
+
+
         ToggleButton mSwitchShowSecure;
         mSwitchShowSecure = (ToggleButton) menu.findItem(R.id.show_secure).getActionView().findViewById(R.id.ToggleButton0111);
         mSwitchShowSecure.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
