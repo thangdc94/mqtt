abff346e2cdfb817f90e4c1e5313d862c94edf1a
thang <thangdc94@gmail.com>
2016-04-23 22:41:37 +0700

    Almost done 2nd

--- app/src/main/java/com/example/am/mqtt/MainActivity.java
+++ app/src/main/java/com/example/am/mqtt/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mDeviceID = "123";
         /**
          * Very important
          */
